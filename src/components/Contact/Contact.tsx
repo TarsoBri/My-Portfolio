f3728177012bd65da058013318dd882e85232783
Tarso Brietzke Iracet <tarsobrietzkeiracet@gmail.com>
2024-04-30 18:41:13 -0300

    Version 1.6

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,5 +1,5 @@
 import "./Contact.scss";
-import { useEffect } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 
 //animation
 import ScrollReveal from "scrollreveal";
@@ -11,6 +11,10 @@ import { FaArrowAltCircleUp } from "react-icons/fa";
 const Contact = () => {
   const whatsappUrl = "https://wa.me/5551998390368?text=Olá Tarso, tudo bem?";
 
+  const [firstName, setFirstName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+
   useEffect(() => {
     ScrollReveal({ reset: true }).reveal(".form", {
       duration: 1000,
@@ -20,25 +24,81 @@ const Contact = () => {
     });
   });
 
+  const handleChangeInputs = (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    switch (name) {
+      case "name":
+        setFirstName(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      case "message":
+        setMessage(value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const handleSubmitForm = (e: FormEvent) => {
+    e.preventDefault();
+
+    setFirstName("");
+    setEmail("");
+    setMessage("");
+  };
+
   return (
     <div id="contact" className="contact_container">
       <div className="contact_content">
         <h2>Contato</h2>
         <div className="form">
-          <form>
+          <form
+            onSubmit={handleSubmitForm}
+            action="https://formsubmit.co/tarsobrietzkeiracet@gmail.com"
+            method="POST"
+          >
             <h3>Me envie uma menssagem</h3>
 
             <div className="contact_content_inputs">
               <label>
-                <input type="text" placeholder="Seu nome" required />
+                <input
+                  type="text"
+                  placeholder="Seu nome"
+                  name="name"
+                  value={firstName}
+                  onChange={handleChangeInputs}
+                  required
+                />
               </label>
               <label>
-                <input type="text" placeholder="Seu email" required />
+                <input
+                  type="text"
+                  placeholder="Seu email"
+                  name="email"
+                  value={email}
+                  onChange={handleChangeInputs}
+                  required
+                />
               </label>
               <label>
-                <textarea placeholder="Mensagem" required rows={6} />
+                <textarea
+                  placeholder="Mensagem"
+                  name="message"
+                  required
+                  value={message}
+                  onChange={handleChangeInputs}
+                  rows={6}
+                />
               </label>
               <input type="submit" className="contact_submit_btn" />
+
+              <input type="hidden" name="_subject" value="Novo Contato!" />
+              <input type="hidden" name="_captcha" value="false" />
+              <input type="text" name="_honey" style={{ display: "none" }} />
             </div>
 
             <h4>Ou</h4>
