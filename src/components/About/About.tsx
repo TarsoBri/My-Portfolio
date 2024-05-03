5a8075dd0f44ecc5356d6df7419163f80ca03876
Tarso Brietzke Iracet <tarsobrietzkeiracet@gmail.com>
2024-05-02 21:16:15 -0300

    Version 2.5

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -44,12 +44,13 @@ const About = () => {
           <div className="about_texts">
             <p>
               Me chamo Tarso, sou desenvolvedor web e estou graduando em Análise
-              e Desenvolvimento de Sistemas na Unisinos. Sou apaixonado com tudo
-              que envolve tecnologia e meu objetivo é me tornar cada vez melhor
-              no que faço, buscando sempre a segurança e a satisfação do
-              cliente, que tal trabalharmos juntos em um projeto? Sou um cara
-              comunicativo e que se da muito bem com as pessoas. Veja meu
-              currículo virtual abaixo para mais detalhes sobre mim.
+              e Desenvolvimento de Sistemas na Unisinos. Sou apaixonado em tudo
+              que envolve tecnologia e meu objetivo é adquirir conhecimentos e
+              praticar cada vez mais para melhorar profissionalmente, buscando
+              sempre a segurança e a satisfação do cliente, que tal trabalharmos
+              juntos em um projeto? Sou um cara comunicativo e que se da muito
+              bem com as pessoas. Veja meu currículo virtual abaixo para mais
+              detalhes sobre mim.
             </p>
             <a href="/Currículo Tarso.pdf" download="Currículo de Tarso Bri">
               CV
